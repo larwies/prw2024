8d7649bf3e8d6ee4b09d91b5a5717c4c7222f243
Larissa <103061974+larwies@users.noreply.github.com>
2024-02-27 20:23:35 -0300

    Update exe1.php

--- exe1.php
+++ exe1.php
@@ -8,59 +8,51 @@
         if ($salario <= 1302.00)
         {
             $salINSS = $salario - (($salario/100) * 7.5);
-        
-            echo "Salário final: ".  $salINSS."\n";
         }
 
         if ($salario <= 2571.29)
         {
             $salINSS = $salario - (($salario/100) * 9);
-        
-            echo "Salário final: ".  $salINSS."\n";
         }    
 
         if ($salario <= 3856.94)
         {
             $salINSS = $salario - (($salario/100) * 12);
-        
-            echo "Salário final: ".  $salINSS."\n";
         }
         
         else
         {
             $salario - (($salario/100) * 14);
-        
-            echo "Salário final: ".  $salINSS."\n";
         }
 
     }
     
         if ($salario <= 1903.98)
         {
-            echo "Salário final: ". $salIRRF."\n";
+            $salIRRF = $salario;
         }
 
         if ($salario <= 2826.65)
         {
             $salIRRF = ($salario - 142.80) - (($salario/100) * 7.5);
-            echo "Salário final: ". $salIRRF."\n";
         }
 
         if ($salario <= 3751.05)
         {
             $salIRRF = ($salario - 354.80) - (($salario/100) * 15);
-            echo "Salário final: ". $salIRRF."\n";
         }
 
         if ($salario <= 4664.68)
         {
             $salIRRF = ($salario - 636.13) - (($salario/100) * 22.5);
-            echo "Salário final: ". $salIRRF."\n";
         }
 
         else
         {
             $salIRRF = ($salario - 869.36) - (($salario/100) * 27.5);
-            echo "Salário final: ". $salIRRF."\n";
         }
+
+         echo "Total a pagar INSS: ".number_format($salINSS,2,".","")." IRPF: ".number_format($salIRRF,2,".","");
+
+
 ?>
