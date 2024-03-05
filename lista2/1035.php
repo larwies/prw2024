ac416c760fa825e1b8df0bb930938f8823791ac1
larwies <farquetelarissa@gmail.com>
2024-03-05 09:39:01 -0300

    lista2

--- lista2/1035.php
+++ lista2/1035.php
@@ -1,9 +1,6 @@
 <?php
 
-    $a = explode(" ",fgets(STDIN));
-    $b = explode(" ",fgets(STDIN));
-    $c = explode(" ",fgets(STDIN));
-    $d = explode(" ",fgets(STDIN));
+    list($a, $b, $c, $d) = explode(' ', readline());
     
     if($b > $c and $d > $a and $c+$d > $a+$b and $c >= 0 and $d >= 0 and $a % 2 == 0)
     {
