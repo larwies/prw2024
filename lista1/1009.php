cb1f133bf35ae24930da994c00a5560e8070cc8f
Larissa <103061974+larwies@users.noreply.github.com>
2024-03-07 20:37:03 -0300

    Create 1009.php

---
+++ lista1/1009.php
@@ -0,0 +1,9 @@
+<?php
+  $nome = readline();
+  $salario = floatval(readline());
+  $venda = floatval(readline());
+  
+  $total = $salario + (15 * $vendas)/100;
+  
+  echo "TOTAL = R$ ".number_format($total,2,".","")."\n";
+?>
