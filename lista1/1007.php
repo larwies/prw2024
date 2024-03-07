51274167ede0e7a64b013694360d13dacaccea79
Larissa <103061974+larwies@users.noreply.github.com>
2024-03-07 20:34:35 -0300

    Create 1007.php

---
+++ lista1/1007.php
@@ -0,0 +1,10 @@
+<?php
+ $a = intval(readline());
+ $b = intval(readline());
+ $c = intval(readline());
+ $d = intval(readline());
+
+ $dif = ($a*$b)-($c*$d);
+ 
+ echo "DIFERENCA = ".$dif."\n";
+?>
