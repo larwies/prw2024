8acf29a161419ce7a0c84610667092104f62101d
Larissa <103061974+larwies@users.noreply.github.com>
2024-03-12 11:21:54 -0300

    Update 1049.php

--- lista2/1049.php
+++ lista2/1049.php
@@ -1,1 +1,70 @@
+<?php
 
+$p1 = readline();
+$p2 = readline();
+$p3 = readline();
+
+switch($p1){
+    case "vertebrado":
+        switch($p2){
+
+            case "ave":
+
+                switch($p3){
+
+                    case "carnivoro":
+                        echo "aguia\n";
+                        break;
+
+                    case "onivoro":
+                        echo"pomba\n";
+                        break;
+                }
+                break;
+
+            case "mamifero":
+
+                switch($p3){
+
+                    case "onivoro":
+                        echo"homem\n";
+                        break;
+
+                    case "herbivoro":
+                        echo"vaca\n";
+                        break;
+                }
+                break;
+        }
+    case "invertebrado":
+
+        switch($p2){
+
+            case "inseto":
+                switch($p3){
+
+                    case "hematofago":
+                        echo"pulga\n";
+                        break;
+
+                    case "herbivoro":
+                        echo"lagarta\n";
+                        break;
+                }
+                break;
+
+            case "anelideo":
+                switch($p3){
+
+                    case "hematofago":
+                        echo"sanguessuga\n";
+                        break;
+
+                    case "onivoro":
+                        echo"minhoca\n";
+                        break;
+                }
+                break;
+        }
+    }
+?>
