05b051bfc8f9afc07ad5b1af27edff33a5c553d9
Larissa <103061974+larwies@users.noreply.github.com>
2024-03-12 11:05:25 -0300

    Update 1048.php

--- lista2/1048.php
+++ lista2/1048.php
@@ -1,1 +1,51 @@
 
+<?php
+
+    $s = floatval (readline());
+      
+    if($s <= 400.00)
+    {
+        $r = ($s * 15)/100;
+        $ns = $s + $r;
+        
+        echo "Novo salario: ".number_format($ns,2,".","")."\n";
+        echo "Reajuste ganho: ".number_format($r,2,".","")."\n";
+        echo "Em percentual: 15 %\n";
+    }
+    else if($s <= 800.00)
+    {
+        $r = ($s * 12)/100;
+        $ns = $s + $r;
+        
+        echo "Novo salario: ".number_format($ns,2,".","")."\n";
+        echo "Reajuste ganho: ".number_format($r,2,".","")."\n";
+        echo "Em percentual: 12 %\n";
+    }
+    else if($s <= 1200.00)
+    {
+        $r = ($s * 10)/100;
+        $ns = $s + $r;
+        
+        echo "Novo salario: ".number_format($ns,2,".","")."\n";
+        echo "Reajuste ganho: ".number_format($r,2,".","")."\n";
+        echo "Em percentual: 10 %\n";
+    }
+    else if($s <= 2000.00)
+    {
+        $r = ($s * 7)/100;
+        $ns = $s + $r;
+        
+        echo "Novo salario: ".number_format($ns,2,".","")."\n";
+        echo "Reajuste ganho: ".number_format($r,2,".","")."\n";
+        echo "Em percentual: 7 %\n";
+    }
+    else if($s > 2000.01)
+    {
+        $r = ($s * 4)/100;
+        $ns = $s + $r;
+        
+        echo "Novo salario: ".number_format($ns,2,".","")."\n";
+        echo "Reajuste ganho: ".number_format($r,2,".","")."\n";
+        echo "Em percentual: 4 %\n";
+    }
+?>
