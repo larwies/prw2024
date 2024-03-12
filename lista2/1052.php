4260960200d40c8842968db8a95d3c74f4888bcc
Larissa <103061974+larwies@users.noreply.github.com>
2024-03-12 11:52:58 -0300

    Create 1052.php

---
+++ lista2/1052.php
@@ -0,0 +1,53 @@
+<?php
+
+    list($n) = explode(' ', readline());
+      
+    if($n == 1)
+    {
+        echo "January\n";
+    }
+    else if($n == 2)
+    {
+        echo "February\n";
+    }
+    else if($n == 3)
+    {
+        echo "March\n";
+    }
+    else if($n == 4)
+    {
+        echo "April\n";
+    }
+    else if($n == 5)
+    {
+        echo "May\n";
+    }
+    else if($n == 6)
+    {
+        echo "June\n";
+    }
+    else if($n == 7)
+    {
+        echo "July\n";
+    }
+    else if($n == 8)
+    {
+        echo "August\n";
+    }
+    else if($n == 9)
+    {
+        echo "September\n";
+    }
+    else if($n == 10)
+    {
+        echo "October\n";
+    }
+    else if($n == 11)
+    {
+        echo "November\n";
+    }
+    else if($n == 12)
+    {
+        echo "December\n";
+    }
+?>
