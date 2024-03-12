80b8ba7335eea97dc9ec19e2e465818335205542
Larissa <103061974+larwies@users.noreply.github.com>
2024-03-12 11:35:46 -0300

    Update 1050.php

--- lista2/1050.php
+++ lista2/1050.php
@@ -1,1 +1,41 @@
+<?php
 
+    list($ddd) = explode(' ', readline());
+      
+    if($ddd == 61)
+    {
+        echo "Brasilia\n";
+    }
+    else if($ddd == 71)
+    {
+        echo "Salvador\n";
+    }
+    else if($ddd == 11)
+    {
+        echo "Sao Paulo\n";
+    }
+    else if($ddd == 21)
+    {
+        echo "Rio de Janeiro\n";
+    }
+    else if($ddd == 32)
+    {
+        echo "Juiz de Fora\n";
+    }
+    else if($ddd == 19)
+    {
+        echo "Campinas\n";
+    }
+    else if($ddd == 27)
+    {
+        echo "Vitoria\n";
+    }
+    else if($ddd == 31)
+    {
+        echo "Belo Horizonte\n";
+    }
+    else 
+    {
+       echo "DDD nao cadastrado\n" 
+    }
+?>
