574c993cfd3472e5d61ff2ff60c3824e98fd63d7
Larissa <103061974+larwies@users.noreply.github.com>
2024-03-16 14:31:20 -0300

    Update 1065.php

--- lista3/1065.php
+++ lista3/1065.php
@@ -1,1 +1,23 @@
+<?php
 
+    $p = 0;
+    
+    while($i < 5)
+    {
+        $v = floatval(readline());
+        
+        if($v%2 == 0)
+        {
+            $p ++;
+            $i ++;
+        
+        }
+        else
+        {
+            $i ++;
+        }
+    }
+    
+    echo $p." valores pares"."\n";
+
+?>
