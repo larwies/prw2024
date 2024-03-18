72aff0666523a2330c5f0247b4095d86369ba591
Larissa <103061974+larwies@users.noreply.github.com>
2024-03-18 18:31:48 -0300

    Create 1078.php

---
+++ lista3/1078.php
@@ -0,0 +1,14 @@
+<?php
+
+    $n = intval(readline());
+    $i = 1;
+
+    {
+        while($i <= 10)
+        {
+            echo "$i x $n = ".$i * $n."\n";
+            $i++;
+        }
+    }
+    
+?>
