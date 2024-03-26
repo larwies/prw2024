1c4698a24629e06abaeb69a077509bf4052877bb
Larissa <103061974+larwies@users.noreply.github.com>
2024-03-26 10:55:46 -0300

    Create 3310.php

---
+++ lista4/3310.php
@@ -0,0 +1,54 @@
+<?php
+    
+    $N = intval(readline());
+    
+    $entradaTeto = explode(" ", readline());
+    $entradaChao = explode(" ", readline());
+    
+    $mudancas = ['-1' => $entradaChao, '1' => $entradaTeto];
+    $mudancaTeto = 0;
+    $mudancaChao = 0;
+    $local = 1;
+    
+    for ($i = 0; $i < $N; $i++) 
+    {
+        if ($mudancas[$local][$i] == 0) 
+        {
+            $mudancaTeto++;
+            $local *= -1;
+        } 
+        elseif ($i != 0) 
+        {
+            if ($mudancas[$local][$i] > $mudancas[$local][$i - 1]) 
+            {
+                $mudancaTeto++;
+                $local *= -1;
+            }
+        }
+    }
+    
+    $local = -1;
+    
+    for ($i = 0; $i < $N; $i++) 
+    {
+        if ($mudancas[$local][$i] == 0) 
+        {
+            $mudancaChao++;
+            $local *= -1;
+        } 
+        elseif ($i != 0) {
+            
+            if ($mudancas[$local][$i] > $mudancas[$local][$i - 1]) 
+            {
+                $mudancaChao++;
+                $local *= -1;
+            }
+        }
+    }
+    
+    if ($mudancaChao > $mudancaTeto)
+        echo "$mudancaTeto\n";
+    else
+        echo "$mudancaChao\n";
+
+?>
