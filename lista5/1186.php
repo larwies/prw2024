f3e189e970ef264e2bb2ef86d7fac90fe85a5502
Larissa <103061974+larwies@users.noreply.github.com>
2024-04-08 20:04:20 -0300

    Create 1186.php

---
+++ lista5/1186.php
@@ -0,0 +1,22 @@
+<?php
+
+    $operacaoChar = readline();
+    $operacao = 0;
+    $matriz = [];
+    
+    for ($i = 0; $i < 12; $i++)
+    
+        for ($j = 0; $j < 12; $j++)
+            $matriz[$i][$j] = floatval(readline());
+    
+    for ($i = 0; $i < 12; $i++)
+    
+        for ($j = 11; $j > 11 - $i; $j--)
+            $operacao += $matriz[$i][$j];
+            
+    if ($operacaoChar == 'M')
+        $operacao /= 66;
+        
+    echo number_format($operacao, 1, '.', '') . "\n";
+
+?>
