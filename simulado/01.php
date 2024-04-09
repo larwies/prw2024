7e4782e9edbcc7fb06a8cec8b268cbf8197696cd
Larissa <103061974+larwies@users.noreply.github.com>
2024-04-09 11:31:51 -0300

    Update 01.php

--- simulado/01.php
+++ simulado/01.php
@@ -1,18 +1,17 @@
+<?php
 $N = intval(readline());
-$jogador = [];
-$tentativa = [0, 0, 0];
-$sucesso = [0, 0, 0];
+$letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 while ($N--) {
-    array_push($jogador, readline());
-    list($saque, $bloqueio, $ataque) = explode(" ", readline());
-    $tentativa[0] += $saque;
-    $tentativa[1] += $bloqueio;
-    $tentativa[2] += $ataque;
-    list($saque, $bloqueio, $ataque) = explode(" ", readline());
-    $sucesso[0] += $saque;
-    $sucesso[1] += $bloqueio;
-    $sucesso[2] += $ataque;
+    $resposta = "";
+    $CT = readline();
+    $letraPos = intval(readline());
+    $CT = str_split($CT);
+    foreach ($CT as $letra) {
+        $local = array_search($letra, $letras);
+        $local -= $letraPos;
+        if ($local < 0)
+            $local = 26 + $local;
+        $resposta .= $letras[$local];
+    }
+    echo "$resposta\n";
 }
-echo "Pontos de Saque: " . number_format(($sucesso[0] / $tentativa[0] * 100), 2, ".", "") . " %.\n";
-echo "Pontos de Bloqueio: " . number_format(($sucesso[1] / $tentativa[1] * 100), 2, ".", "") . " %.\n";
-echo "Pontos de Ataque: " . number_format(($sucesso[2] / $tentativa[2] * 100), 2, ".", "") . " %.\n";
