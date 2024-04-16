39fd0c1e91d950d89894c8a31d10974d1ed13db0
Larissa <103061974+larwies@users.noreply.github.com>
2024-04-16 10:19:11 -0300

    Update 01.php

--- simulado/01.php
+++ simulado/01.php
@@ -1,17 +1,13 @@
 <?php
-$N = intval(readline());
-$letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-while ($N--) {
-    $resposta = "";
-    $CT = readline();
-    $letraPos = intval(readline());
-    $CT = str_split($CT);
-    foreach ($CT as $letra) {
-        $local = array_search($letra, $letras);
-        $local -= $letraPos;
-        if ($local < 0)
-            $local = 26 + $local;
-        $resposta .= $letras[$local];
-    }
-    echo "$resposta\n";
-}
+
+  $velocidade = intval(readline());
+  $distancia = intval(readline());
+  $consumo = intval(readline());
+  
+  $th = $distancia/$velocidade;
+  $tc = $distancia/$consumo;
+  
+  echo "Total horas: ".number_format($th,2,".","")."\n";
+  echo "Total combustível: ".number_format($tc,2,".","")."\n";
+  
+?>
